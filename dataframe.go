dc3365a1548eb03017ca874c0ba0cfc67d52c1a3
王布衣 <wangfengxy@sina.cn>
2023-02-01 13:55:26 +0000

    !9 #I6C994: 支持dataframe选一段子集, 目前暂不支持索引, 只能按照行号区间来选择 * #I6C994: 支持dataframe选一段子集, 目前暂不支持索引, 只能按照行号区间来选择

--- dataframe.go
+++ dataframe.go
@@ -228,28 +228,6 @@ func (df DataFrame) print(
 	return str
 }
 
-// Subset returns a subset of the rows of the original DataFrame based on the
-// Series subsetting indexes.
-func (df DataFrame) Subset(start, end int) DataFrame {
-	if df.Err != nil {
-		return df
-	}
-	columns := make([]Series, df.ncols)
-	for i, column := range df.columns {
-		s := column.Subset(start, end)
-		columns[i] = s
-	}
-	nrows, ncols, err := checkColumnsDimensions(columns...)
-	if err != nil {
-		return DataFrame{Err: err}
-	}
-	return DataFrame{
-		columns: columns,
-		ncols:   ncols,
-		nrows:   nrows,
-	}
-}
-
 func checkColumnsDimensions(se ...Series) (nrows, ncols int, err error) {
 	ncols = len(se)
 	nrows = -1
