4b50a7d11ca7c6f9f2dae40f32c3b0a65a7ae5a5
王布衣 <wangfengxy@sina.cn>
2023-02-05 07:08:42 +0000

    !25 #I6CXOV 增加float32类型的series, 增加REF函数 * Merge branch 'ref' of https://gitee.com/quant1x/pandas into ref * 实现REF函数 * 实现REF函数 * 新增float32数据类型 * 新增float32数据类型 * 调整shift函数为独立一个源文件 * 暴露为公共函数 * 从变量名上明确int是64位的int64 * 删除废弃的代码

---
+++ formula/ref_test.go
@@ -0,0 +1,41 @@
+package formula
+
+import (
+	"fmt"
+	"gitee.com/quant1x/pandas"
+	"testing"
+)
+
+func TestREF(t *testing.T) {
+	type testStruct struct {
+		A string
+		B int
+		C bool
+		D float32
+	}
+	data := []testStruct{
+		{"a", 1, true, 0.0},
+		{"b", 2, false, 0.5},
+	}
+	df1 := pandas.LoadStructs(data)
+	fmt.Println(df1)
+	// 修改列名
+	_ = df1.SetNames("a", "b", "c", "d")
+	// 增加1列
+	s_e := pandas.GenericSeries[string]("", "a0", "a1", "a2", "a3")
+	df2 := df1.Join(s_e)
+	fmt.Println(df2)
+	A := df2.Col("a")
+	B := df2.Col("b")
+	C := df2.Col("c")
+	D := df2.Col("d")
+
+	// 2日均线
+	r2 := REF(D, 2)
+	fmt.Println(r2)
+
+	_ = A
+	_ = B
+	_ = C
+	_ = D
+}
