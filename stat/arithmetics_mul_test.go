5b8859ed162999260d76a652253f9e8df3c921e5
wangfeng <wangfengxy@sina.cn>
2023-02-13 11:20:05 +0800

    #I6EMN5 实现乘法

---
+++ stat/arithmetics_mul_test.go
@@ -0,0 +1,18 @@
+package stat
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMul(t *testing.T) {
+	f1 := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	f2 := []float32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	d2 := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	d3 := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	d4 := []int64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	fmt.Println(Mul(f1, f2))
+	fmt.Println(Mul(d2, float64(1)))
+	fmt.Println(Mul(d3, int32(2)))
+	fmt.Println(Mul(d4, int64(3)))
+}
