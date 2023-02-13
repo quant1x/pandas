991d207d937a28501ae3163195944baedd154472
王布衣 <wangfengxy@sina.cn>
2023-02-13 03:23:17 +0000

    !53 #I6EMN5 实现乘法 * #I6EMN5 实现乘法 * #I6EMN5 实现乘法

---
+++ stat/arithmetics_mul.go
@@ -0,0 +1,20 @@
+package stat
+
+import (
+	"github.com/viterin/vek"
+	"github.com/viterin/vek/vek32"
+	"golang.org/x/exp/slices"
+)
+
+// Mul arithmetics 乘法
+func Mul[T Number](x []T, y any) []T {
+	return binaryOperations(x, y, vek32.Mul, vek.Mul, __mul_go[T])
+}
+
+func __mul_go[T Number](x, y []T) []T {
+	x = slices.Clone(x)
+	for i := 0; i < len(x); i++ {
+		x[i] *= y[i]
+	}
+	return x
+}
