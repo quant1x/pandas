9704e605efa208da2413d3541d256d03c2eff65a
王布衣 <wangfengxy@sina.cn>
2023-02-17 00:54:27 +0000

    !65 #I6CC21 实现MACD指标 * #I6CC21 实现MACD指标

---
+++ indicator/macd_test.go
@@ -0,0 +1,14 @@
+package indicator
+
+import (
+	"fmt"
+	"gitee.com/quant1x/pandas/data/cache"
+	"testing"
+)
+
+func TestMACD(t *testing.T) {
+	df := cache.KLine("sz002528")
+	fmt.Println(df)
+	df1 := MACD(df, 5, 13, 3)
+	fmt.Println(df1)
+}
