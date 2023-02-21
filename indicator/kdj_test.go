8771b018273db6490e72813d55b85c97101f5923
wangfeng <wangfengxy@sina.cn>
2023-02-21 16:01:34 +0800

    修订data package路径

--- indicator/kdj_test.go
+++ indicator/kdj_test.go
@@ -2,12 +2,12 @@ package indicator
 
 import (
 	"fmt"
-	"gitee.com/quant1x/data/cache"
+	"gitee.com/quant1x/data/stock"
 	"testing"
 )
 
 func TestKDJ(t *testing.T) {
-	df := cache.KLine("sz002528")
+	df := stock.KLine("sz002528")
 	fmt.Println(df)
 	df1 := KDJ(df, 9, 3, 3)
 	fmt.Println(df1)
--- indicator/macd_test.go
+++ indicator/macd_test.go
@@ -2,12 +2,12 @@ package indicator
 
 import (
 	"fmt"
-	"gitee.com/quant1x/data/cache"
+	"gitee.com/quant1x/data/stock"
 	"testing"
 )
 
 func TestMACD(t *testing.T) {
-	df := cache.KLine("sz002528")
+	df := stock.KLine("sz002528")
 	fmt.Println(df)
 	df1 := MACD(df, 5, 13, 3)
 	fmt.Println(df1)
