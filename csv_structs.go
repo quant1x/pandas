6a74cc4c6163297ff893b2ffb4e219321082e204
wangfeng <wangfengxy@sina.cn>
2023-06-10 11:21:12 +0800

    调整csv组件依赖库

--- csv_structs.go
+++ csv_structs.go
@@ -3,23 +3,53 @@ package pandas
 import (
 	"gitee.com/quant1x/gox/api"
 	"gitee.com/quant1x/gox/util/homedir"
-	"gitee.com/quant1x/pandas/internal/csvreader"
 	"github.com/gocarina/gocsv"
 	"os"
 )
 
-// Csv2Slices CSV文件转struct切片
-func Csv2Slices[S ~[]E, E any](filename string, pointer *S) error {
+//// Csv2Slices CSV文件转struct切片
+//func Csv2Slices[S ~[]E, E any](filename string, pointer *S) error {
+//	filepath, err := homedir.Expand(filename)
+//	if err != nil {
+//		return err
+//	}
+//	decoder := csvreader.New()
+//	return decoder.UnMarshalFile(filepath, pointer)
+//}
+//
+//// Slices2Csv struct切片保存csv文件
+//func Slices2Csv[S ~[]E, E any](filename string, s S) error {
+//	filepath, err := homedir.Expand(filename)
+//	if err != nil {
+//		return err
+//	}
+//	// 检查目录, 不存在就创建
+//	_ = api.CheckFilepath(filepath, true)
+//	csvFile, err := os.Create(filepath)
+//	if err != nil {
+//		return err
+//	}
+//	defer api.CloseQuietly(csvFile)
+//	err = gocsv.MarshalFile(s, csvFile)
+//	return err
+//}
+
+// CsvToSlices CSV文件转struct切片
+func CsvToSlices[S ~[]E, E any](filename string, pointer *S) error {
 	filepath, err := homedir.Expand(filename)
 	if err != nil {
 		return err
 	}
-	decoder := csvreader.New()
-	return decoder.UnMarshalFile(filepath, pointer)
+	csvFile, err := os.Open(filepath)
+	if err != nil {
+		return err
+	}
+	err = gocsv.Unmarshal(csvFile, pointer)
+	return err
 }
 
-// Slices2Csv struct切片保存csv文件
-func Slices2Csv[S ~[]E, E any](filename string, s S) error {
+// SlicesToCsv struct切片转csv文件
+func SlicesToCsv[S ~[]E, E any](filename string, s S) error {
 	filepath, err := homedir.Expand(filename)
 	if err != nil {
 		return err
