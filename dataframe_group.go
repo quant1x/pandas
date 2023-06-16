980b01d765eab5e955dd72ec9b1524c20127b61e
wangfeng <wangfengxy@sina.cn>
2023-06-16 11:31:23 +0800

    拆分group和filter, 为后续优化做准备

--- dataframe_group.go
+++ dataframe_group.go
@@ -7,6 +7,11 @@ import (
 
 // Group 分组
 func (self DataFrame) Group(columnName string, filter func(kind stat.Type, e any) bool) DataFrame {
+	return self.Filter(columnName, filter)
+}
+
+// Filter 过滤
+func (self DataFrame) Filter(columnName string, filter func(kind stat.Type, e any) bool) DataFrame {
 	series := self.Col(columnName)
 	if series.Len() == 0 {
 		return self
