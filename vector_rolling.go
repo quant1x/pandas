94e5888d8193e76d655640da067a94318185885f
wangfeng <wangfengxy@sina.cn>
2024-02-26 10:58:26 +0800

    优化Series的Rolling函数

--- vector_rolling.go
+++ vector_rolling.go
@@ -3,17 +3,23 @@ package pandas
 import "gitee.com/quant1x/num"
 
 func (this vector[T]) Rolling(param any) RollingAndExpandingMixin {
+	window := num.Any2Window[num.DType](param)
+	w := RollingAndExpandingMixin{
+		Window: window,
+		Series: this,
+	}
+	return w
+}
+
+func (this vector[T]) v1Rolling(param any) RollingAndExpandingMixin {
 	window := num.Window[num.DType]{C: num.NaN()}
 	switch v := param.(type) {
 	case int:
-		//N = num.Repeat[num.DType](num.DType(v), this.Len())
 		window.C = num.DType(v)
 	case []num.DType:
-		//N = num.Align(v, num.DTypeNaN, this.Len())
 		window.V = v
 	case Series:
 		vs := v.DTypes()
-		//N = num.Align(vs, num.DTypeNaN, this.Len())
 		window.V = vs
 	default:
 		panic(num.ErrInvalidWindow)
@@ -25,22 +31,22 @@ func (this vector[T]) Rolling(param any) RollingAndExpandingMixin {
 	return w
 }
 
-//func (this vector[T]) Rolling(param any) RollingAndExpandingMixin {
-//	var N []num.DType
-//	switch v := param.(type) {
-//	case int:
-//		N = num.Repeat[num.DType](num.DType(v), this.Len())
-//	case []num.DType:
-//		N = num.Align(v, num.DTypeNaN, this.Len())
-//	case Series:
-//		vs := v.DTypes()
-//		N = num.Align(vs, num.DTypeNaN, this.Len())
-//	default:
-//		panic(num.ErrInvalidWindow)
-//	}
-//	w := RollingAndExpandingMixin{
-//		Window: N,
-//		Series: this,
-//	}
-//	return w
-//}
+func (this vector[T]) v2Rolling(param any) RollingAndExpandingMixin {
+	var N []num.DType
+	switch v := param.(type) {
+	case int:
+		N = num.Repeat[num.DType](num.DType(v), this.Len())
+	case []num.DType:
+		N = num.Align(v, num.NaN(), this.Len())
+	case Series:
+		vs := v.DTypes()
+		N = num.Align(vs, num.NaN(), this.Len())
+	default:
+		panic(num.ErrInvalidWindow)
+	}
+	w := RollingAndExpandingMixin{
+		Window: num.Window[num.DType]{V: N},
+		Series: this,
+	}
+	return w
+}
