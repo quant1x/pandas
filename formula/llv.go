f36afb1f730d4cd91c3914e3944272f7ea40bfde
wangfeng <wangfengxy@sina.cn>
2024-02-26 12:43:29 +0800

    优化llv函数

--- formula/llv.go
+++ formula/llv.go
@@ -1,10 +1,47 @@
 package formula
 
 import (
+	"gitee.com/quant1x/num"
 	"gitee.com/quant1x/pandas"
 )
 
 // LLV 最近N周期的S最小值
 func LLV(S pandas.Series, N any) pandas.Series {
+	return v2LLV(S, N)
+}
+
+func v1LLV(S pandas.Series, N any) pandas.Series {
 	return S.Rolling(N).Min()
+}
+
+func v2LLV(S pandas.Series, N any) pandas.Series {
+	x := S.Values()
+	switch vs := x.(type) {
+	case []int32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int32) int32 {
+			return num.Min2(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []int64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int64) int64 {
+			return num.Min2(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float32) float32 {
+			return num.Min2(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float64) float64 {
+			return num.Min2(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []string:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...string) string {
+			return num.Min2(values)
+		})
+		return pandas.SliceToSeries(d)
+	}
+	panic(num.ErrUnsupportedType)
 }
--- formula/llv_test.go
+++ formula/llv_test.go
@@ -2,11 +2,14 @@ package formula
 
 import (
 	"fmt"
+	"gitee.com/quant1x/num"
+	"gitee.com/quant1x/num/labs"
 	"gitee.com/quant1x/pandas"
+	"slices"
 	"testing"
 )
 
-func TestLLV(t *testing.T) {
+func TestLLV_basic(t *testing.T) {
 	type testStruct struct {
 		A string
 		B int
@@ -43,4 +46,63 @@ func TestLLV(t *testing.T) {
 	_ = B
 	_ = C
 	_ = D
+}
+
+func TestLLV(t *testing.T) {
+	type args struct {
+		S pandas.Series
+		N any
+	}
+	tests := []struct {
+		name string
+		args args
+		want pandas.Series
+	}{
+		{
+			name: "llv",
+			args: args{
+				S: pandas.NewSeriesWithType(pandas.SERIES_TYPE_FLOAT64, "x", []float64{2, 1, 3, 4, 5, 2}),
+				N: 3,
+			},
+			want: pandas.NewSeriesWithType(pandas.SERIES_TYPE_FLOAT64, "x", []float64{num.Float64NaN(), num.Float64NaN(), 1, 1, 3, 2}),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := LLV(tt.args.S, tt.args.N); !labs.DeepEqual(got.Values(), tt.want.Values()) {
+				t.Errorf("LLV() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkLLV_init(b *testing.B) {
+	testDataOnce.Do(initTestData)
+}
+
+func BenchmarkLLV_release(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		LLV(s, 10)
+	}
+}
+
+func BenchmarkLLV_v1(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v1LLV(s, 10)
+	}
+}
+
+func BenchmarkLLV_v2(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v2LLV(s, 10)
+	}
 }
