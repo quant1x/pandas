97146a952d5453f5a88f36036b2130e564ffb686
wangfeng <wangfengxy@sina.cn>
2024-02-26 12:40:00 +0800

    优化sum函数

--- formula/sum.go
+++ formula/sum.go
@@ -1,6 +1,7 @@
 package formula
 
 import (
+	"gitee.com/quant1x/num"
 	"gitee.com/quant1x/pandas"
 )
 
@@ -8,5 +9,36 @@ import (
 // 如果N=0, 则从第一个有效值累加到当前
 // 下一步再统一返回值
 func SUM(S pandas.Series, N any) pandas.Series {
+	return v2SUM(S, N)
+}
+
+func v1SUM(S pandas.Series, N any) pandas.Series {
 	return S.Rolling(N).Sum()
+}
+
+func v2SUM(S pandas.Series, N any) pandas.Series {
+	x := S.Values()
+	switch vs := x.(type) {
+	case []int32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int32) int32 {
+			return num.Sum(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []int64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int64) int64 {
+			return num.Sum(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float32) float32 {
+			return num.Sum(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float64) float64 {
+			return num.Sum(values)
+		})
+		return pandas.SliceToSeries(d)
+	}
+	panic(num.ErrUnsupportedType)
 }
--- formula/sum_test.go
+++ formula/sum_test.go
@@ -2,11 +2,14 @@ package formula
 
 import (
 	"fmt"
+	"gitee.com/quant1x/num"
+	"gitee.com/quant1x/num/labs"
 	"gitee.com/quant1x/pandas"
+	"slices"
 	"testing"
 )
 
-func TestSUM(t *testing.T) {
+func TestSUM_basic(t *testing.T) {
 	f0 := []float64{1.1, 2.2, 1.3, 1.4}
 	f1 := []float64{70, 80, 75, 83, 86}
 	f2 := []float64{90, 69, 60, 88, 87}
@@ -15,6 +18,121 @@ func TestSUM(t *testing.T) {
 	s1 := pandas.NewSeriesWithoutType("f1", f1)
 	s2 := pandas.NewSeriesWithoutType("f2", f2)
 	fmt.Println(SUM(s0, 4))
-	fmt.Println(SUM(s1, 5))
-	fmt.Println(SUM(s2, 5))
+	fmt.Println(v1SUM(s1, 5))
+	fmt.Println(v2SUM(s2, 5))
+}
+
+func TestSUM_release(t *testing.T) {
+	type args struct {
+		S pandas.Series
+		N any
+	}
+	tests := []struct {
+		name string
+		args args
+		want pandas.Series
+	}{
+		{
+			name: "float64",
+			args: args{
+				S: pandas.ToVector[float64](1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+				N: 5,
+			},
+			want: pandas.ToVector[float64](num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), 15, 20, 25, 30, 35, 40),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SUM(tt.args.S, tt.args.N); !labs.DeepEqual(got.Values(), tt.want.Values()) {
+				t.Errorf("SUM() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSUM_v1(t *testing.T) {
+	type args struct {
+		S pandas.Series
+		N any
+	}
+	tests := []struct {
+		name string
+		args args
+		want pandas.Series
+	}{
+		{
+			name: "float64",
+			args: args{
+				S: pandas.ToVector[float64](1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+				N: 5,
+			},
+			want: pandas.ToVector[float64](num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), 15, 20, 25, 30, 35, 40),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v1SUM(tt.args.S, tt.args.N); !labs.DeepEqual(got.Values(), tt.want.Values()) {
+				t.Errorf("v1SUM() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSUM_v2(t *testing.T) {
+	type args struct {
+		S pandas.Series
+		N any
+	}
+	tests := []struct {
+		name string
+		args args
+		want pandas.Series
+	}{
+		{
+			name: "float64",
+			args: args{
+				S: pandas.ToVector[float64](1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+				N: 5,
+			},
+			want: pandas.ToVector[float64](num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), 15, 20, 25, 30, 35, 40),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := v2SUM(tt.args.S, tt.args.N); !labs.DeepEqual(got.Values(), tt.want.Values()) {
+				t.Errorf("v2SUM() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func BenchmarkSUM_init(b *testing.B) {
+	testDataOnce.Do(initTestData)
+}
+
+func BenchmarkSUM_release(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		SUM(s, 10)
+	}
+}
+
+func BenchmarkSUM_v1(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v1SUM(s, 10)
+	}
+}
+
+func BenchmarkSUM_v2(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v2SUM(s, 10)
+	}
 }
