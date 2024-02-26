bce24bbc2d199ff2ff95eb02e092c694987dfcdf
wangfeng <wangfengxy@sina.cn>
2024-02-26 12:40:39 +0800

    优化std函数

--- formula/std.go
+++ formula/std.go
@@ -1,10 +1,42 @@
 package formula
 
 import (
+	"gitee.com/quant1x/num"
 	"gitee.com/quant1x/pandas"
 )
 
 // STD 序列的N日标准差
 func STD(S pandas.Series, N any) pandas.Series {
+	return v2STD(S, N)
+}
+
+func v1STD(S pandas.Series, N any) pandas.Series {
 	return S.Rolling(N).Std()
+}
+
+func v2STD(S pandas.Series, N any) pandas.Series {
+	x := S.Values()
+	switch vs := x.(type) {
+	case []int32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int32) int32 {
+			return num.Std(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []int64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...int64) int64 {
+			return num.Std(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float32:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float32) float32 {
+			return num.Std(values)
+		})
+		return pandas.SliceToSeries(d)
+	case []float64:
+		d := num.RollingV1(vs, N, func(N num.DType, values ...float64) float64 {
+			return num.Std(values)
+		})
+		return pandas.SliceToSeries(d)
+	}
+	panic(num.ErrUnsupportedType)
 }
--- formula/std_test.go
+++ formula/std_test.go
@@ -5,6 +5,7 @@ import (
 	"gitee.com/quant1x/num"
 	"gitee.com/quant1x/num/labs"
 	"gitee.com/quant1x/pandas"
+	"slices"
 	"testing"
 )
 
@@ -24,32 +25,60 @@ func TestSTD_basic(t *testing.T) {
 }
 
 func TestSTD(t *testing.T) {
-	f0 := []float64{1.1, 2.2, 1.3, 1.4}
-	f1 := []float64{70, 80, 75, 83, 86}
-	f2 := []float64{90, 69, 60, 88, 87}
-
-	s0 := pandas.NewSeriesWithoutType("f0", f0)
-	s1 := pandas.NewSeriesWithoutType("f1", f1)
-	s2 := pandas.NewSeriesWithoutType("f2", f2)
-	fmt.Println(STD(s0, 4))
-	fmt.Println(STD(s1, 5))
-	fmt.Println(STD(s2, 5))
-}
-
-func BenchmarkSTD_release(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		x := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-		s := pandas.Vector(x)
-		r := STD(s, 5)
-		_ = r
+	type args struct {
+		S pandas.Series
+		N any
+	}
+	tests := []struct {
+		name string
+		args args
+		want pandas.Series
+	}{
+		{
+			name: "float64",
+			args: args{
+				S: pandas.ToVector[float64](1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+				N: 5,
+			},
+			want: pandas.ToVector[float64](num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), num.Float64NaN(), 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951, 1.4142135623730951),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := STD(tt.args.S, tt.args.N); !labs.DeepEqual(got.Values(), tt.want.Values()) {
+				t.Errorf("STD() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
 
-func BenchmarkSTD_v3Rolling(b *testing.B) {
+func BenchmarkSTD_init(b *testing.B) {
+	testDataOnce.Do(initTestData)
+}
+
+func BenchmarkSTD_release(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
 	for i := 0; i < b.N; i++ {
-		x := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-		num.RollingV1(x, 5, func(N num.DType, values ...float64) float64 {
-			return num.Std(values)
-		})
+		STD(s, 10)
+	}
+}
+
+func BenchmarkSTD_v1(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v1STD(s, 10)
+	}
+}
+
+func BenchmarkSTD_v2(b *testing.B) {
+	testDataOnce.Do(initTestData)
+	f64s := slices.Clone(testDataFloat64)
+	s := pandas.SliceToSeries(f64s)
+	for i := 0; i < b.N; i++ {
+		v2STD(s, 10)
 	}
 }
